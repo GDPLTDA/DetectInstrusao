67aaae8d0e0d0b68c1a3da0848311b895db313e1
Rodrigo Mendonça da Paixão <rodrigo-mendonca@outlook.com.br>
2015-08-31 00:16:28 -0300

    arquivo de configuracao do vagrant

---
+++ source/IDS/bootstrap.sh
@@ -0,0 +1,15 @@
+#update inicial
+sudo apt-get update
+
+#basico
+
+sudo apt-get install -y vim 2> /dev/null
+
+sudo apt-get install -y apache2 2> /dev/null
+
+# golang
+sudo apt-get install -y python-software-properties 2> /dev/null
+sudo add-apt-repository ppa:duh/golang
+sudo apt-get update
+sudo apt-get install -y golang 2> /dev/null
+
